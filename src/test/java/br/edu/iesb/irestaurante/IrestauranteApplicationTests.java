d1c51681ce8426e060f2f6871edbad78da2fb921
araketu <diogo.ghizoni@outlook.com>
2021-04-20 19:48:04 -0300

    Primeiro Commit API - Criaçao do Projeto

---
+++ src/test/java/br/edu/iesb/irestaurante/IrestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iesb.irestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
