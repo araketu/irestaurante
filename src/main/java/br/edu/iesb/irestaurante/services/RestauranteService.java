5acb0318edb12d18fab073834b2cab3e61026503
EDiSOUSA1997 <dini.badboy@hotmail.com>
2021-04-21 15:04:14 -0300

    implementação post delete update

--- src/main/java/br/edu/iesb/irestaurante/services/RestauranteService.java
+++ src/main/java/br/edu/iesb/irestaurante/services/RestauranteService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.iesb.irestaurante.domain.Restaurante;
+import br.edu.iesb.irestaurante.dto.RestauDTO;
 import br.edu.iesb.irestaurante.repository.RestauranteRepository;
 
 @Service
@@ -24,6 +25,41 @@ public class RestauranteService {
 	public Restaurante findById(String id) {
 		Optional<Restaurante> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Restaurante nao Encontrado"));
+	}
+	
+//	Metodo para incluir um restaurante 
+	public Restaurante insert(Restaurante obj) {
+		return repo.insert(obj);
+	}
+	
+//	Metodo para excluir Restaurantes
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+//	Metodo para atualizar o objeto - restaurante
+	public Restaurante update(Restaurante obj) {
+		Restaurante newObj = repo.findById(obj.getId()).get();
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	
+	private void updateData(Restaurante newObj, Restaurante obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		newObj.setNumero(obj.getNumero());
+		newObj.setCidade(obj.getCidade());
+		newObj.setBairro(obj.getBairro());
+		newObj.setEndereco(obj.getEndereco());
+		newObj.setTipocomida(obj.getTipocomida());
+			
+	}
+	
+//	Metodo que retorna um Restaurante com os atributos do RestauDTO
+	public Restaurante fromDTO(RestauDTO objDto) {
+		return new Restaurante(objDto.getId(), objDto.getNome() , null, null, objDto.getCidade(), objDto.getBairro(), null, objDto.getTipocomida());
 	}
 
 }
